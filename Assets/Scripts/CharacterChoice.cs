506cbf422d25b806cd7a707ecdfb186b9054c8e5
Ilytopia <loryncesur@gmail.com>
2026-04-15 11:20:51 +0200

    KindaWorkingCharacterSelections
    No lock if a character is already choosen

--- Assets/Scripts/CharacterChoice.cs
+++ Assets/Scripts/CharacterChoice.cs
@@ -1,10 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class CharacterChoice : MonoBehaviour
 {
     private Player _player1;
     private Player _player2;
+    
+    private InputAction _clickAction;
+    private Camera _camera;
+    
+    private List<ChoosableCharacter> _choosableCharacters = new List<ChoosableCharacter>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,12 +20,49 @@ public class CharacterChoice : MonoBehaviour
         _player2 = GameObject.FindGameObjectWithTag("Player2").GetComponent<Player>();
         
         _player2.enabled = false;
+        
+        _clickAction = InputSystem.actions.FindAction("Attack");
+        _camera = Camera.main;
+
+        foreach (ChoosableCharacter choosable_character in FindObjectsByType<ChoosableCharacter>(FindObjectsSortMode.None))
+        {
+            _choosableCharacters.Add(choosable_character);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_clickAction.WasPressedThisFrame())
+        {
+            ChooseCharacter();
+        }
+    }
+
+    private void ChooseCharacter()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(_camera.ScreenToWorldPoint(Mouse.current.position.ReadValue()), Vector3.forward);
+
+        if (!hit)
+        {
+            // Debug.Log("No Hit");
+            return;
+        }
+
+        foreach (ChoosableCharacter choosable_character in _choosableCharacters)
+        {
+            if (choosable_character.gameObject == hit.collider.gameObject)
+            {
+                if (_player1.enabled)
+                {
+                    _player1.SetSprite(choosable_character.GetSpriteRenderer().sprite);
+                }
+                else
+                {
+                    _player2.SetSprite(choosable_character.GetSpriteRenderer().sprite);
+                }
+            }
+        }
     }
 
     public void PreviousStep()
