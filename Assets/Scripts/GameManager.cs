571a00b4fc969e5d1ca04fc8c3067e53a92f592b
Ilytopia <loryncesur@gmail.com>
2026-05-02 19:14:16 +0200

    Spawn VFX on click

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.VFX;
 
 public class GameManager : MonoBehaviour
 {
+    private InputAction _clickAction;
+
+    [SerializeField] private GameObject _clickVFX;
+    private VisualEffect _visualEffect;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _clickAction = InputSystem.actions.FindAction("Attack");
+        _visualEffect = _clickVFX.GetComponent<VisualEffect>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_clickAction.WasPressedThisFrame())
+        {
+            _clickVFX.transform.position = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            _clickVFX.transform.position = new  Vector3(_clickVFX.transform.position.x, _clickVFX.transform.position.y, -1.0f);
+            _visualEffect.Reinit();
+            _visualEffect.Play();
+        }
     }
 
     public void StartGameLocal()
--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -1,9 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using UnityEngine.VFX;
 using Random = System.Random;
 
 public class GameplayManager : MonoBehaviour
@@ -22,11 +20,6 @@ public class GameplayManager : MonoBehaviour
     
     private GameManager _gameManager;
     
-    private InputAction _clickAction;
-    private Camera _camera;
-
-    [SerializeField] private VisualEffect _clickVFX;
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -37,9 +30,6 @@ public class GameplayManager : MonoBehaviour
         {
             throw new NotImplementedException();
         }
-        
-        _clickAction = InputSystem.actions.FindAction("Attack");
-        _camera = Camera.main;
         
         // _gameButtons = FindObjectsByType<GameGridDetections>(FindObjectsInactive.Exclude, FindObjectsSortMode.InstanceID);
 
@@ -74,11 +64,7 @@ public class GameplayManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_clickAction.WasPressedThisFrame())
-        {
-            RaycastHit2D hit = Physics2D.Raycast(_camera.ScreenToWorldPoint(Mouse.current.position.ReadValue()), Vector3.forward);
-            
-        }
+        
     }
 
     public void ChangeTurn()
